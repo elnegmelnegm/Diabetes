ee16190130370d5eab9c2f15e206c3aefe559ee6
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-03 19:22:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from pathlib import Path
+from PIL import Image
 import google.generativeai as genai
 
 # Configure the API key
@@ -31,26 +32,33 @@ input_prompt = """
                As an expert specializing in assessing the suitability of fruits and foods for individuals with diabetes, your task involves analyzing input images featuring various food items. Your first objective is to identify the type of fruit or food present in the image. Subsequently, you must determine the glycemic index of the identified item. Based on this glycemic index, provide recommendations on whether individuals with diabetes can include the detected food in their diet. If the food is deemed suitable, specify the recommended quantity for consumption. Use English and Arabic languages for the response.
                """
 
-def input_image_setup(file_loc):
-    if not (img := Path(file_loc)).exists():
-        raise FileNotFoundError(f"Could not find image: {img}")
-
-    image_parts = [
-        {"mime_type": "image/jpeg", "data": Path(file_loc).read_bytes()}
-    ]
-    return image_parts
-
-def generate_gemini_response(image_loc):
-    image_prompt = input_image_setup(image_loc)
-    prompt_parts = [input_prompt, image_prompt[0]]
-
-    # Generate response
-    try:
-        response = model.generate_content(prompt_parts)
-        return response.text
-    except Exception as e:
-        st.error(f"Error generating response: {e}")
+def input_image_setup(uploaded_file):
+    if not uploaded_file:
+        st.error("No file uploaded.")
         return None
+
+    try:
+        # Read the content of the uploaded file as bytes
+        image_parts = [
+            {"mime_type": "image/jpeg", "data": uploaded_file.read()}
+        ]
+        return image_parts
+    except Exception as e:
+        st.error(f"Error reading uploaded file: {e}")
+        return None
+
+def generate_gemini_response(uploaded_file):
+    image_prompt = input_image_setup(uploaded_file)
+    if image_prompt:
+        prompt_parts = [input_prompt, image_prompt[0]]
+        
+        try:
+            response = model.generate_content(prompt_parts)
+            return response.text
+        except Exception as e:
+            st.error(f"Error generating response: {e}")
+
+    return None
 
 # Display header
 st.set_page_config(
@@ -58,8 +66,10 @@ st.set_page_config(
     page_icon="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png",
     layout="wide",
 )
+
 st.markdown('''
 <img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
+
 st.markdown('''
 Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://png2.cleanpng.com/sh/38d322d41e2d6d5738e129190b8c33a7/L0KzQYq3VsI0N5Ruf5H0aYP2gLBuTgB6fJl0hp9sb33zhcXskr1qa5Dzi595cnBqgrL0jflvb15xedDwdXHqdX7smPVkfaVmRadtMHazcbKAg8c5bpM4RqI9MUS7Q4e4UcU3OWM7TqoANUi0R4W1kP5o/kisspng-python-computer-icons-programming-language-executa-5d0f0aa7c78fb3.0414836115612668558174.png" width="22" height="22">''', unsafe_allow_html=True)
 
@@ -68,9 +78,9 @@ uploaded_file = st.file_uploader(label="Upload an image of your food", type=["jp
 
 if uploaded_file:
     try:
-        response_text = generate_gemini_response(uploaded_file)
+        response = generate_gemini_response(uploaded_file)
         st.text("Uploaded File: " + uploaded_file.name)
         st.text("Generated Response:")
-        st.write(response_text)
+        st.write(response)
     except Exception as e:
         st.error(f"Error processing image: {e}")
