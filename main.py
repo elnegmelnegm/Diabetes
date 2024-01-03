e3800b40ede6a4f229d908301b363816a8012c33
elnegmelnegm <48047676+elnegmelnegm@users.noreply.github.com>
2024-01-03 20:32:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -71,7 +71,7 @@ st.markdown('''
 <img src="https://www.edaegypt.gov.eg/media/wc3lsydo/group-287.png" width="250" height="100">''', unsafe_allow_html=True)
 
 st.markdown('''
-Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src="https://i.ibb.co/8z1Cnqr/nn.png" width="22" height="22">''', unsafe_allow_html=True)
+Powered by Google AI <img src="https://seeklogo.com/images/G/google-ai-logo-996E85F6FD-seeklogo.com.png" width="20" height="20"> Streamlit <img src="https://global.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png" width="22" height="22"> Python <img src= "https://i.ibb.co/wwCs096/nn-1-removebg-preview-removebg-preview.png" width="22" height="22">''', unsafe_allow_html=True)
 
 # File upload and response display
 uploaded_file = st.file_uploader(label="Upload an image of your food", type=["jpg", "jpeg", "png"])
